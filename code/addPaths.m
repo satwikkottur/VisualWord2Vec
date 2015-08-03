fd10862ab790437de1b8d795cbaead27ddd317b2
Satwik Kottur <satwik27@gmail.com>
2015-08-03 12:04:03 -0400

    Clustering, visualiation using t-sne done

--- code/addPaths.m
+++ code/addPaths.m
@@ -1,8 +1,8 @@
 % Adding the paths
-root = '/home/satwik/VisualWord2Vec/code';
-addpath(genpath(fullfile(root, 'src/')));
-addpath(genpath(fullfile(root, 'io/')));
-addpath(genpath(fullfile(root, 'utils/')));
-addpath(genpath(fullfile(root,  'debug/')));
+root = '/home/satwik/VisualWord2Vec';
+addpath(genpath(fullfile(root, 'code/src/')));
+addpath(genpath(fullfile(root, 'code/io/')));
+addpath(genpath(fullfile(root, 'code/utils/')));
+addpath(genpath(fullfile(root,  'code/debug/')));
 
 addpath(genpath(fullfile(root , 'libs')));
--- code/clustering/ClusteringDemo.m
+++ code/clustering/ClusteringDemo.m
@@ -14,21 +14,28 @@ numFeaturePath = fullfile(dataPath, 'Num_features.txt');
 [Plabel, Slabel, Rlabel, Rfeatures] = readFromFile(psrFeaturePath, numFeaturePath);
 
 % Assigning the cluster ids (each relation word is a cluster)
-[clusterR, ~, clusterIds] = unique(Rlabel);
+[clusterR, ~, uniqIds] = unique(Rlabel);
 
 % Visualization
 noDims = 2;
 noInitDims = 50;
 perplexity = 30;
-tsnePts = tsne(Rfeatures, clusterIds, noDims, noInitDims, perplexity);
+tsnePts = tsne(Rfeatures, uniqIds, noDims, noInitDims, perplexity);
+
+figure; gscatter(tsnePts(:, 1), tsnePts(:, 2), uniqIds);
 
 % Clustering the features
-
-
+% yael k-means
+% Usage: yael_kmeans(vectors, noClusters)
+noClusters = 10;
+[clusterCentres, clusterIds] = yael_kmeans(single(Rfeatures)', noClusters);
 
 % Assigning the cluster ids after clustering
-
 % Visualization
+figure; gscatter(tsnePts(:, 1), tsnePts(:, 2), clusterIds);
 
 time = toc;
 fprintf('Total time taken : %f\n', time);
+
+% Saving the session
+save('clusteringSession_Aug3.mat');
