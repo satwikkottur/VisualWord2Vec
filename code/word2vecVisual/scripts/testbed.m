3244e277b13d5e2ea4f84da0230b38814d14e526
Satwik Kottur <satwik27@gmail.com>
2015-08-15 17:41:34 -0400

    Folder structure for developing cluster based refining added

---
+++ code/word2vecVisual/scripts/testbed.m
@@ -0,0 +1,9 @@
+% Reading the relations
+
+% Reading the word2vec embeddings
+
+% Finding the word2vec embeddings for the relations
+
+% Visualizing through t-sne
+
+% Visualizing the embedding after refining based on clusters
