c3f442cd61210679a986147debfc0e2b580c9442
Satwik Kottur <satwik27@gmail.com>
2015-10-01 17:28:56 -0400

    Visualization addeds, need to debug errors

--- code/word2vecVisual/scripts/commonSenseAnalysis/analyzeCommonSenseTask.m
+++ code/word2vecVisual/scripts/commonSenseAnalysis/analyzeCommonSenseTask.m
@@ -27,12 +27,17 @@ end
 [sortedTuples, sortInd] = sortImprovedTuples(tuples);
 
 % Print first N tuples
-noExp = 30;
+noExp = 100;
 featDim = 200;
+
+% Collect unique embeddings for top sorted tuples
+% Consider P,R,S separately
 % Collect the embeddings for top sorted tuples
-preCollect = zeros(3 * noExp, featDim);
-postCollect = zeros(3 * noExp, featDim);
-labels = cell(3 * noExp, 1);
+p
+uniqPreP = containers.Map(); uniqPostP = containers.Map();
+uniqPreR = containers.Map(); uniqPostR = containers.Map();
+uniqPreS = containers.Map(); uniqPostS = containers.Map();
+
 for i = 1:noExp
     if verbose
         fprintf('<%s , %s , %s> : %f %f\n', char(sortedTuples{1}(i)), ...
@@ -42,27 +47,62 @@ for i = 1:noExp
                                             sortedTuples{5}(i));
     end
 
-    preCollect(3*(i-1) + (1:3), :) = [preEmbed.P(char(sortedTuples{1}(i))); ...
-                                        preEmbed.R(char(sortedTuples{2}(i))); ...
-                                        preEmbed.S(char(sortedTuples{3}(i)))];
+    p = char(sortedTuples{1}(i));
+    r = char(sortedTuples{2}(i));
+    s = char(sortedTuples{3}(i));
 
-    postCollect(3*(i-1) + (1:3), :) = [postEmbed.P(char(sortedTuples{1}(i))); ...
-                                        postEmbed.R(char(sortedTuples{2}(i))); ...
-                                        postEmbed.S(char(sortedTuples{3}(i)))];
-    labels(3 *(i-1) + (1:3)) = {char(sortedTuples{1}(i)); ...
-                                        char(sortedTuples{2}(i)); ...
-                                        char(sortedTuples{3}(i))};
+    % P
+    % New label, add it
+    if(~uniqPreP.isKey(p))
+        uniqPreP(p) = preEmbed.P(p);
+        uniqPostP(p) = postEmbed.P(p);
+    end
+
+    % R
+    if(~uniqPreR.isKey(r))
+        uniqPreR(r) = preEmbed.R(r);
+        uniqPostR(r) = postEmbed.R(r);
+    end
+
+    % S
+    if(~uniqPreS.isKey(s))
+        uniqPreS(s) = preEmbed.S(s);
+        uniqPostS(s) = postEmbed.S(s);
+    end
 end
 
+% Now collect the embeddings
+preCollectP = [];
+preCollectS = [];
+preCollectR = [];
+postCollectP = [];
+postCollectS = [];
+postCollectR = [];
+for i = 1:
 % Embed through t-sne
 noDims = 2;
-noInitDims = 20;
-perplexity = 50;
-tsnePre = tsne(preCollect, [], noDims, noInitDims, perplexity);
-tsnePost = tsne(postCollect, [], noDims, noInitDims, perplexity);
+noInitDims = 50;
+perplexity = 5;
+tsnePre = tsne([cell2mat(uniqPreP.values'); ...
+                cell2mat(uniqPreS.values'); ...
+                cell2mat(uniqPreR.values')],...
+                [], noDims, noInitDims, perplexity);
 
-%figure(1); scatter(tsnePre(:, 1), tsnePre(:, 2))
-%figure(2); scatter(tsnePost(:, 1), tsnePost(:, 2))
-figure(1); text(tsnePre(:, 1), tsnePre(:, 2), labels)
-figure(2); text(tsnePost(:, 1), tsnePost(:, 2), labels)
+tsnePost = tsne([cell2mat(uniqPostP.values'); ...
+                cell2mat(uniqPostS.values'); ...
+                cell2mat(uniqPostR.values')],...
+                [], noDims, noInitDims, perplexity);
+
+labels = [uniqPreP.keys, uniqPreS.keys, uniqPreR.keys];
+groups = [ones(length(uniqPreP.keys), 1); ...
+        2 * ones(length(uniqPreS.keys), 1); ...
+        3 * ones(length(uniqPreR.keys), 1)];
+
+% Displacement for points
+dx = 0.1; dy = 0.1;
+figure(1); gscatter(tsnePre(:, 1), tsnePre(:, 2), groups)
+text(tsnePre(:, 1) + dx, tsnePre(:, 2) + dy, labels)
+
+figure(2); gscatter(tsnePost(:, 1), tsnePost(:, 2), groups)
+text(tsnePost(:, 1) + dx, tsnePost(:, 2) + dy, labels)
 % Read
