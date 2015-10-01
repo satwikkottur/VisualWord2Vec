160c310b073814878bccf60c440e30c434661baa
Satwik Kottur <satwik27@gmail.com>
2015-10-01 18:41:30 -0400

    Visualization done for now. Need better ways to interpret

--- code/word2vecVisual/scripts/commonSenseAnalysis/analyzeCommonSenseTask.m
+++ code/word2vecVisual/scripts/commonSenseAnalysis/analyzeCommonSenseTask.m
@@ -27,16 +27,22 @@ end
 [sortedTuples, sortInd] = sortImprovedTuples(tuples);
 
 % Print first N tuples
+%noExp = length(sortInd);
 noExp = 100;
 featDim = 200;
 
 % Collect unique embeddings for top sorted tuples
 % Consider P,R,S separately
 % Collect the embeddings for top sorted tuples
-p
-uniqPreP = containers.Map(); uniqPostP = containers.Map();
-uniqPreR = containers.Map(); uniqPostR = containers.Map();
-uniqPreS = containers.Map(); uniqPostS = containers.Map();
+pLabels = {};
+rLabels = {};
+sLabels = {};
+uniqPreP = zeros(noExp, featDim);
+uniqPreR = zeros(noExp, featDim);
+uniqPreS = zeros(noExp, featDim);
+uniqPostP = zeros(noExp, featDim);
+uniqPostR = zeros(noExp, featDim);
+uniqPostS = zeros(noExp, featDim);
 
 for i = 1:noExp
     if verbose
@@ -53,56 +59,85 @@ for i = 1:noExp
 
     % P
     % New label, add it
-    if(~uniqPreP.isKey(p))
-        uniqPreP(p) = preEmbed.P(p);
-        uniqPostP(p) = postEmbed.P(p);
+    if(~ismember(p, pLabels))
+        pLabels = [pLabels, {p}];
+        uniqPreP(length(pLabels), :) = preEmbed.P(p);
+        uniqPostP(length(pLabels), :) = postEmbed.P(p);
     end
 
     % R
-    if(~uniqPreR.isKey(r))
-        uniqPreR(r) = preEmbed.R(r);
-        uniqPostR(r) = postEmbed.R(r);
+    if(~ismember(r, rLabels))
+        rLabels = [rLabels, {r}];
+        uniqPreR(length(rLabels), :) = preEmbed.R(r);
+        uniqPostR(length(rLabels), :) = postEmbed.R(r);
     end
 
     % S
-    if(~uniqPreS.isKey(s))
-        uniqPreS(s) = preEmbed.S(s);
-        uniqPostS(s) = postEmbed.S(s);
+    if(~ismember(s, sLabels))
+        sLabels = [sLabels, {s}];
+        uniqPreS(length(sLabels), :) = preEmbed.S(s);
+        uniqPostS(length(sLabels), :) = postEmbed.S(s);
     end
 end
 
-% Now collect the embeddings
-preCollectP = [];
-preCollectS = [];
-preCollectR = [];
-postCollectP = [];
-postCollectS = [];
-postCollectR = [];
-for i = 1:
-% Embed through t-sne
-noDims = 2;
-noInitDims = 50;
-perplexity = 5;
-tsnePre = tsne([cell2mat(uniqPreP.values'); ...
-                cell2mat(uniqPreS.values'); ...
-                cell2mat(uniqPreR.values')],...
-                [], noDims, noInitDims, perplexity);
+% Trim the matrices
+uniqPreP = uniqPreP(1:length(pLabels), :);
+uniqPreR = uniqPreR(1:length(rLabels), :);
+uniqPreS = uniqPreS(1:length(sLabels), :);
 
-tsnePost = tsne([cell2mat(uniqPostP.values'); ...
-                cell2mat(uniqPostS.values'); ...
-                cell2mat(uniqPostR.values')],...
-                [], noDims, noInitDims, perplexity);
+uniqPostP = uniqPostP(1:length(pLabels), :);
+uniqPostR = uniqPostR(1:length(rLabels), :);
+uniqPostS = uniqPostS(1:length(sLabels), :);
 
-labels = [uniqPreP.keys, uniqPreS.keys, uniqPreR.keys];
-groups = [ones(length(uniqPreP.keys), 1); ...
-        2 * ones(length(uniqPreS.keys), 1); ...
-        3 * ones(length(uniqPreR.keys), 1)];
+% Embed through t-sne (either individual / together)
+individual = false;
+if(individual)
+    noDims = 2;
+    noInitDims = [];
+    perplexity = 50;
 
-% Displacement for points
-dx = 0.1; dy = 0.1;
-figure(1); gscatter(tsnePre(:, 1), tsnePre(:, 2), groups)
-text(tsnePre(:, 1) + dx, tsnePre(:, 2) + dy, labels)
+    tsnePreP = tsne(uniqPreP, [], noDims, noInitDims, perplexity);
+    tsnePostP = tsne(uniqPostP, [], noDims, noInitDims, perplexity);
 
-figure(2); gscatter(tsnePost(:, 1), tsnePost(:, 2), groups)
-text(tsnePost(:, 1) + dx, tsnePost(:, 2) + dy, labels)
-% Read
+    tsnePreS = tsne(uniqPreS, [], noDims, noInitDims, perplexity);
+    tsnePostS = tsne(uniqPostS, [], noDims, noInitDims, perplexity);
+
+    tsnePreR = tsne(uniqPreR, [], noDims, noInitDims, perplexity);
+    tsnePostR = tsne(uniqPostR, [], noDims, noInitDims, perplexity);
+
+    % Displacement for points
+    dx = 0.1; dy = 0.1;
+    figure(1); scatter(tsnePreP(:, 1), tsnePreP(:, 2))
+    text(tsnePreP(:, 1) + dx, tsnePreP(:, 2) + dy, pLabels)
+    figure(2); scatter(tsnePostP(:, 1), tsnePostP(:, 2))
+    text(tsnePostP(:, 1) + dx, tsnePostP(:, 2) + dy, pLabels)
+
+    figure(3); scatter(tsnePreS(:, 1), tsnePreS(:, 2))
+    text(tsnePreS(:, 1) + dx, tsnePreS(:, 2) + dy, sLabels)
+    figure(4); scatter(tsnePostS(:, 1), tsnePostS(:, 2))
+    text(tsnePostS(:, 1) + dx, tsnePostS(:, 2) + dy, sLabels)
+
+    figure(5); scatter(tsnePreR(:, 1), tsnePreR(:, 2))
+    text(tsnePreR(:, 1) + dx, tsnePreR(:, 2) + dy, rLabels)
+    figure(6); scatter(tsnePostR(:, 1), tsnePostR(:, 2))
+    text(tsnePostR(:, 1) + dx, tsnePostR(:, 2) + dy, rLabels)
+else
+    noDims = 2;
+    noInitDims = 50;
+    perplexity = 5;
+
+    tsnePre = tsne([uniqPreP; uniqPreS; uniqPreR], [], noDims, noInitDims, perplexity);
+    tsnePost = tsne([uniqPostP; uniqPostS; uniqPostR], [], noDims, noInitDims, perplexity);
+
+    labels = [pLabels, sLabels, rLabels];
+    groups = [ones(length(pLabels), 1); ...
+            2 * ones(length(sLabels), 1); ...
+            3 * ones(length(rLabels), 1)];
+
+    % Displacement for points
+    dx = 0.1; dy = 0.1;
+    figure(7); gscatter(tsnePre(:, 1), tsnePre(:, 2), groups)
+    text(tsnePre(:, 1) + dx, tsnePre(:, 2) + dy, labels)
+    figure(8); gscatter(tsnePost(:, 1), tsnePost(:, 2), groups)
+    text(tsnePost(:, 1) + dx, tsnePost(:, 2) + dy, labels)
+end
