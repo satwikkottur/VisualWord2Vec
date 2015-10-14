e19f145857b5e255f398a02fc2ea99aba588095f
Satwik Kottur <satwik27@gmail.com>
2015-10-14 12:18:10 -0400

    Lemmatizing code corrected for lemmatizing P,S

--- code/word2vecVisual/scripts/LemmatizePRStuples.py
+++ code/word2vecVisual/scripts/LemmatizePRStuples.py
@@ -1,6 +1,7 @@
 # Script to lemmatize P,R,S tuples that were changed manually
 import json
 import re
+from nltk.stem import WordNetLemmatizer
 
 # Read the map
 jsonPath = '/home/satwik/VisualWord2Vec/data/spellings_relations.json';
@@ -17,11 +18,20 @@ psrPath = '/home/satwik/VisualWord2Vec/data/PSR_features.txt';
 psrFile = open(psrPath, 'rb');
 psrLines = [re.split(':', i.strip('<>\n')) for i in psrFile.readlines()];
 
+lmt = WordNetLemmatizer();
 # Correct each tuple, if applicable
 for tupl in psrLines:
-    for j in tupl:
-        if(j in inverseMap):
-            psrLines[psrLines.index(tupl)][tupl.index(j)] = inverseMap[j] 
+    # Find R in the inversemap but lemmatize P,S as nouns
+    # P
+    psrLines[prsLines.index(tupl)][0] = lmt.lemmatize(tupl[0]);
+
+    # S
+    psrLines[prsLines.index(tupl)][1] = lmt.lemmatize(tupl[1]);
+
+    # R
+    r = tupl[2];
+    if r in inverseMap:
+        psrLines[prsLines.index(tupl)][2] = inverseMap[r];
 
 # Write to new file
 savePath = '/home/satwik/VisualWord2Vec/data/PSR_features_lemma.txt';
--- code/word2vecVisual/scripts/correctInflections.py
+++
@@ -1,2 +0,0 @@
-# Script to correct the inflections for P,R,S in the training instances
-
