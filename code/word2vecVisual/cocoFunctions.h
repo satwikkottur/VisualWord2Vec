31578fd21b864b0d10f9624e58f0fc6f9bcfc678
Satwik Kottur <satwik27@gmail.com>
2015-10-27 15:03:00 -0400

    Added cocoFunctions.h/.c; cleaned Makefile-looks elegant

---
+++ code/word2vecVisual/cocoFunctions.h
@@ -0,0 +1,22 @@
+// Header for running COCO scripts
+// Just a wrapper for existing functions, got together to make 
+// MS COCO training work
+
+# ifndef COCO
+# define COCO
+
+// Standard libraries
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <math.h>
+# include <pthread.h>
+# include <ctype.h>
+
+# include "structs.h"
+# include "macros.h"
+# include "helperFunctions.h"
+# include "refineFunctions.h"
+
+
+#endif
